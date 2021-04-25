e0b59363a127ee1e767019bb8e487238e843966e
andrewshwets <andrewshwets@gmail.com>
2021-04-25 22:57:42 +0300

    Initial commit for Command AI

---
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CommandAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IEnumerable<string>> GetCommandsList()
+        {
+            IEnumerable<string> list = await Task.FromResult<string[]>(new string[] { "this", "is", "hard", "coded", "text" });
+
+            return list;
+        }
+    }
+}
+
