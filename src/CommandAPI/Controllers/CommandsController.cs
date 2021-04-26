4ed567f2d8b9f1a5337656a6162a7f5e417ddf3c
andrewshwets <andrewshwets@gmail.com>
2021-04-26 11:57:48 +0300

    Add Commands model

--- src/CommandAPI/Controllers/CommandsController.cs
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CommandAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -15,9 +16,15 @@ namespace CommandAPI.Controllers
         [HttpGet]
         public async Task<IEnumerable<string>> GetCommandsList()
         {
-            IEnumerable<string> list = await Task.FromResult<string[]>(new string[] { "this", "is", "hard", "coded", "text" });
+            IEnumerable<string> list = await Task.FromResult(new string[] { "this", "is", "hard", "coded", "text" });
 
             return list;
+        }
+
+        [HttpPost]
+        public async Task CreateCommand([FromBody] Command command)
+        {
+            Console.WriteLine($"Id: {command.Id}.\nHowTo: {command.HowTo}.\nCommandLine: {command.CommandLine}.");
         }
     }
 }
